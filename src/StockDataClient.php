7593aafa47d46f659c903a54de67e57a03e5a4c8
David Piesse <piesse@gmail.com>
2015-11-23 15:52:26 +0000

    small updates

--- src/StockDataClient.php
+++ src/StockDataClient.php
@@ -4,7 +4,7 @@ class StockDataClient
 {
     //all the functions to get data in a laravel sort of way
 //    protected $client = new Schub
-
+    private static $client;
     //pass in single string or array
     public static function getStockList($stocks){
         $client = new \Scheb\YahooFinanceApi\ApiClient();
